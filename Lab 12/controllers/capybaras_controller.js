274558fad77f2aec5253f0c21d6bb03b7233f943
Jorge Guerrero <jorge.guerrero.0023@gmail.com>
2022-03-09 11:07:52 -0600

    Lab 14

--- Lab 12/controllers/capybaras_controller.js
+++ Lab 12/controllers/capybaras_controller.js
@@ -9,41 +9,59 @@ exports.cerveza = (request, response, next) => {
 //MOVIES
 exports.get_nuevo = (request, response, next) => {
     console.log('GET /capybaras/nuevo');
-    response.render('nuevo', {nombre: 'Lalo'}); 
+    response.render('nuevo', {
+        username:request.session.username ? request.session.username:'',
+        info:''
+    }); 
 };
 exports.post_nuevo = (request, response, next) => {
     console.log('POST /capybaras/nuevo');
     console.log(request.body);
     const capybara = new Capybara(request.body.nombre);
     capybara.save();
+    request.session.info=capybara.nombre+'fue registrado con éxito';
+    response.setHeader('Ser-Cookie','ultimo_capybara='+capybara.nombre+'; HttpOnly');
     response.redirect('/capybaras');
 };
 ////SEIRES
 exports.get_nuevo2=(request, response, next) => {
     console.log('GET /capybaras/nuevo2');
-    response.render('nuevo2', {nombre: 'Lalo'}); 
+    response.render('nuevo2', {username:request.session.username ? request.session.username:'',
+    info:''}); 
 };
 exports.post_nuevo2=(request, response, next) => {
     console.log('POST /capybaras/nuevo2');
     console.log(request.body);
     const capybaras2=new Capybara2(request.body.nombre);
     capybaras2.save();
+    request.session.info=capybaras2.nombre+'fue registrado con éxito';
+    response.setHeader('Ser-Cookie','ultimo_capybara='+capybaras2.nombre+'; HttpOnly');
     response.redirect('/capybaras');
 };
 //////ANIME
 exports.get_nuevo3=(request, response, next) => {
     console.log('GET /capybaras/nuevo3');
-    response.render('nuevo3', {nombre: 'Lalo'}); 
+    response.render('nuevo3', {username:request.session.username ? request.session.username:'',
+    info:''}); 
 };
 exports.post_nuevo3=(request, response, next) => {
     console.log('POST /capybaras/nuev3');
     console.log(request.body);
     const capybaras3= new Capybara3(request.body.nombre);
     capybaras3.save();
+    request.session.info=capybaras3.nombre+'fue registrado con éxito';
+    response.setHeader('Ser-Cookie','ultimo_capybara='+capybaras3.nombre+'; HttpOnly');
     response.redirect('/capybaras');
 };
 ///////
 exports.listar = (request, response, next) => {
     console.log('Ruta /capybaras');
-    response.render('lista', {capybaras: Capybara.fetchAll(), capybaras2: Capybara2.fetchAll(), capybaras3:Capybara3.fetchAll()}); 
+    console.log(request.cookies);
+    const info= request.session.info ? request.session.info :'';
+    request.session.info='';
+    response.render('lista', {capybaras: Capybara.fetchAll(), capybaras2: Capybara2.fetchAll(), capybaras3:Capybara3.fetchAll(),
+    username:request.session.username ? request.session.username:'',
+    ultimo_capybara:request.cookies.ultimo_capybara ? request.cookies.ultimo_capybara:'',
+    info:info
+    }); 
 }
